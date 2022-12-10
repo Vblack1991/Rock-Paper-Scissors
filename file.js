213f692cbdec2012b4dae143984a44a2e67ff2d6
Vincent <v_blackshear@yahoo.com>
2022-12-09 17:39:23 -0800

    enabled the printing of score sums

--- file.js
+++ file.js
@@ -1,12 +1,10 @@
-let playerScore = 0;
-let computerScore = 0;
-let draw = 0;
-
+//random choice generator for computer
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
+//value comparisons for each round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "Scissors" 
     || playerSelection === "paper" && computerSelection === "Rock" 
@@ -21,11 +19,14 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+//variables for round results and score
 let roundForPlayer = "You've won this round"
 let roundForComputer = "Computer wins this round"
 let Draw = "Draw,play again"
 let playerWin = "Congrats,you've won the game!"
 let computerWin = "You lost the game!"
+let playerScore = 0;
+let computerScore = 0;
 
 for (let i = 0; i < 5; i++) {
     let playerSelection = prompt("Enter your choice").toLowerCase();
@@ -33,36 +34,18 @@ for (let i = 0; i < 5; i++) {
     let roundResult = playRound(playerSelection, computerSelection);
     game(roundResult);
     console.log(roundResult);
-    if (roundForPlayer === 3)
-        console.log(playerWin);
-    else if (roundForComputer === 3)
-        console.log(computerWin);
-    else (Draw > 4)
-        console.log(Draw);
+    if (roundResult === roundForPlayer) {
+        playerScore++;
+    } else if (roundResult === roundForComputer) {
+        computerScore++;
+    }
+    let Scores = [playerScore, computerScore];
+        console.log(Scores);
 }
 
 function game() {
-    if (roundForPlayer === 5)
-        return playerWin;
-    else if (roundForComputer === 5)
-        return computerWin;
-    else (Draw === 5)
-        return Draw;
+    if (playerScore === computerScore + 2)
+        console.log(playerWin);
+    if (computerScore === playerScore + 2)
+        console.log(computerWin);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
