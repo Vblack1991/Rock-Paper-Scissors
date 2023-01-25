828602a5044c84edf6e68e41d29629dc18897f70
Vincent <v_blackshear@yahoo.com>
2023-01-24 16:27:05 -0800

    replace all console logs with dom methods and remove 5 game limit

--- file.js
+++ file.js
@@ -1,71 +1,112 @@
 //random choice generator for computer
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
+//button for each selection
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+
+//variables for round results and score display
+let roundForPlayer = "You've won this round";
+let roundForComputer = "Computer wins this round";
+let Draw = "Draw,play again";
+let pScore = 0;
+let cScore = 0;
+let dCount = 0;
+document.getElementById('playerScore').textContent = 'Player:' + pScore;
+document.getElementById('computerScore').textContent = 'Computer:' + cScore;
+document.getElementById('drawCount').textContent = 'Draws:' + dCount;
+
+//event listeners for each choice w/score keeping
+rockButton.addEventListener('click', function() {
+    let playerSelection = 'rock';
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    let roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult === roundForPlayer) {
+        document.getElementById('winner').textContent = roundForPlayer;
+        pScore++;
+    } else if (roundResult === roundForComputer) {
+        document.getElementById('winner').textContent = roundForComputer;
+        cScore++;
+    } else if (roundResult === Draw) {
+        document.getElementById('winner').textContent = Draw;
+        dCount++;
+    }
+    game(pScore, cScore);
+    
+    document.getElementById('playerScore').textContent = 'Player:' + pScore;
+    document.getElementById('computerScore').textContent = 'Computer:' + cScore;
+    document.getElementById('drawCount').textContent = 'Draws:' + dCount;
+});
+
+paperButton.addEventListener('click', function() {
+    let playerSelection = 'paper';
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    let roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult === roundForPlayer) {
+        document.getElementById('winner').textContent = roundForPlayer;
+        pScore++;
+    } else if (roundResult === roundForComputer) {
+        document.getElementById('winner').textContent = roundForComputer;
+        cScore++;
+    } else if (roundResult === Draw) {
+        document.getElementById('winner').textContent = Draw;
+        dCount++;
+    }
+    game(pScore, cScore);
+    
+    document.getElementById('playerScore').textContent = 'Player:' + pScore;
+    document.getElementById('computerScore').textContent = 'Computer:' + cScore;
+    document.getElementById('drawCount').textContent = 'Draws:' + dCount;
+});
+
+scissorsButton.addEventListener('click', function() {
+    let playerSelection = 'scissors';
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    let roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult === roundForPlayer) {
+        document.getElementById('winner').textContent = roundForPlayer;
+        pScore++;
+    } else if (roundResult === roundForComputer) {
+        document.getElementById('winner').textContent = roundForComputer;
+        cScore++;
+    } else if (roundResult === Draw) {
+        document.getElementById('winner').textContent = Draw;
+        dCount++;
+    }
+    game(pScore, cScore);
+
+    document.getElementById('playerScore').textContent = 'Player:' + pScore;
+    document.getElementById('computerScore').textContent = 'Computer:' + cScore;
+    document.getElementById('drawCount').textContent = 'Draws:' + dCount;
+});
+
 //value comparisons for each round
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "Scissors" 
-    || playerSelection === "paper" && computerSelection === "Rock" 
-    || playerSelection === "scissors" && computerSelection === "Paper") {
+    if (playerSelection === 'rock' && computerSelection === 'Scissors' 
+    || playerSelection === 'paper' && computerSelection === 'Rock' 
+    || playerSelection === 'scissors' && computerSelection === 'Paper') {
         return roundForPlayer;
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock"
-    || playerSelection === "paper" && computerSelection === "Scissors"
-    || playerSelection === "rock" && computerSelection === "Paper") {
+    } else if (playerSelection === 'scissors' && computerSelection === 'Rock'
+    || playerSelection === 'paper' && computerSelection === 'Scissors'
+    || playerSelection === 'rock' && computerSelection === 'Paper') {
         return roundForComputer;
     } else {
         return Draw;
     }
-}
+};
 
-//variables for round results and score
-let roundForPlayer = "You've won this round";
-let roundForComputer = "Computer wins this round";
-let Draw = "Draw,play again";
-let playerWin = "Congrats,you've won the game!";
-let computerWin = "You lost the game!";
-let playerScore = 0;
-let computerScore = 0;
-let drawCount = 0;
-
-//loop to print round and game results
-for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Enter your choice").toLowerCase();
-    let computerSelection = getComputerChoice();
-    let roundResult = playRound(playerSelection, computerSelection);
-    game(roundResult);
-    console.log(roundResult);
-    if (roundResult === roundForPlayer) {
-        playerScore++;
-    } else if (roundResult === roundForComputer) {
-        computerScore++;
-    } else if (roundResult === Draw) {
-        drawCount++;
+//function that announces winner
+function game(pScore, cScore) {
+    if (pScore === 5) {
+        return document.getElementById('winner').textContent = "Congrats,you've won the game!";
+    } else if (cScore === 5) {
+        return document.getElementById('winner').textContent = "Haha, you lost the game!";
     }
-    if (i > 3 && playerScore > computerScore) {
-        console.log(playerWin);
-    } else if (i > 3 && computerScore > playerScore) {
-        console.log(computerWin);
-    } else if (i > 3 && computerScore === playerScore) {
-        console.log(Draw);
-    }
-    if (playerScore === 3) {
-        console.log(playerWin);
-        break;
-    }
-    if (computerScore === 3) {
-        console.log(computerWin);
-        break;
-    }
-    let Scores = [playerScore, computerScore];
-        console.log(Scores);
-}
-
-//game function
-function game() {
-
-if (drawCount === 5) {
-    console.log(Draw);
-}
-}
+};
